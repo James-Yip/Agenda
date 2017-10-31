3d11077b0b114feba138b071c58db9471f3cec2e
张力越 <haswelliris@live.com>
2017-11-01 02:52:03 +0800

    utils.go重命名为util.go 死妈powershell环境变量失效  go的脑残import机制

---
+++ util/util.go
@@ -0,0 +1,70 @@
+package util
+import "bytes"
+import "strings"
+// format: yyyy-mm-dd/hh:mm
+func time2str(timeStr string) string {
+	var a = ""
+	var buff =  bytes.NewBufferString(a)
+	
+	for i:=0;i<16;i++ {
+		if(i!=4&&i!=7&&i!=10&&i!=13) {
+			buff.WriteByte(timeStr[i-1])
+		}
+	}
+	a=buff.String()
+	return a;
+}
+
+func str2time(time string) string {
+	var a = ""
+	var buff =  bytes.NewBufferString(a)
+	for i:=0;i<16;i++ {
+		switch i {
+		case 4,7:
+			buff.WriteByte('-')
+		case 5,6:
+			buff.WriteByte(time[i-1])
+		case 8,9:
+			buff.WriteByte(time[i-2])
+		case 10:
+			buff.WriteByte('/')
+		case 11,12:
+			buff.WriteByte(time[i-3])
+		case 13:
+			buff.WriteByte(':')
+		case 14,15:
+			buff.WriteByte(time[i-4])
+		default:
+			buff.WriteByte(time[i])
+		}
+	}
+	a=buff.String()
+	return a;
+}
+
+func isTimeValid(time string) bool {
+	a := false
+	if (len(time)!=16) {
+		return a
+	}
+	for i:=0;i<16;i++ {
+		switch i {
+		case 4,7:
+			if time[i]!='-' { return a }
+		case 10:
+			if time[i]!='/'{ return a }
+		case 13:
+			if time[i]!=':'{ return a }
+		default:
+			if (time[i]< '0' || time[i]> '9' ) { return a }
+		}
+	}
+	a=false
+	return a
+}
+
+// input: "a b c"
+// output: [a b c]
+func str2slice(participator_str string) []string {
+	return strings.Fields(participator_str)
+}
