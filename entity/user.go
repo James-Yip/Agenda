b4f0379ceea9086a3026d7b6694cddd0d6e623b7
James <874244887@qq.com>
2017-11-01 13:32:20 +0800

    finish pkg entity

--- entity/user.go
+++ entity/user.go
@@ -1,5 +1,13 @@
 package entity
 
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
 type User struct {
 	UserName string
 	Password string
@@ -10,20 +18,96 @@ type User struct {
 type Users []*User
 
 var users Users
-var meetings Meetings
 
-func (users *Users) AddUser(userName string, password string, email string, phone string) {
-
+func AddUser(user *User) {
+	users = append(users, user)
+	userWriteToFile()
 }
 
-func DeleteUser(userName string) {
-
+func DeleteUser(userName string) error {
+	index, _, err := GetUser(userName)
+	if err != nil {
+		return errors.New("delete user fail: " + err.Error())
+	} else {
+		// delete user with given index
+		users = append(users[:index], users[index+1:]...)
+		userWriteToFile()
+	}
+	return nil
 }
 
-func GetUser(userName string) {
+func GetUser(userName string) (int, *User, error) {
+	for index, user := range users {
+		if user.UserName == userName {
+			return index, user, nil
+		}
+	}
+	return -1, nil, errors.New("can not get user by " + userName)
+}
 
+func GetUsers() Users {
+	return users
+}
+
+func userEncode(user *User) ([]byte, error) {
+	encodedUser, err := json.Marshal(user)
+	if err != nil {
+		return nil, errors.New("user encode fail: " + err.Error())
+	}
+	return encodedUser, nil
+}
+
+func userDecode(encodedUser []byte) (*User, error) {
+	var user User
+	err := json.Unmarshal(encodedUser, &user)
+	if err != nil {
+		return nil, errors.New("user decode fail: " + err.Error())
+	}
+	return &user, nil
+}
+
+func userWriteToFile() error {
+	fout, err := os.Create("data/userInfo")
+	if err != nil {
+		return errors.New("write to file fail: \n->" + err.Error())
+	}
+	defer fout.Close()
+
+	for _, user := range users {
+		encodedUser, err := userEncode(user)
+		if err != nil {
+			return errors.New("write to file fail: \n->" + err.Error())
+		}
+		fmt.Fprintf(fout, "%s\n", encodedUser)
+	}
+
+	return nil
+}
+
+func userReadFromFile() error {
+	fin, err := os.Open("data/userInfo")
+	if err != nil {
+		return errors.New("user read from file fail: \n->" + err.Error())
+	}
+	defer fin.Close()
+
+	scanner := bufio.NewScanner(fin)
+
+	for scanner.Scan() {
+		token := scanner.Text()
+		user, err := userDecode([]byte(token))
+		if err != nil {
+			return errors.New("user read from file fail: \n->" + err.Error())
+		}
+		users = append(users, user)
+	}
+
+	return nil
 }
 
 func init() {
-
+	err := userReadFromFile()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
 }
