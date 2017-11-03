6e72181d1b0b75477dbbbb719cc31f76f1ea678f
张力越 <haswelliris@live.com>
2017-11-03 23:58:31 +0800

    test

--- uTest.go
+++ uTest.go
@@ -4,6 +4,6 @@ import fuck "agenda/utils"
 import "fmt"
 func main() {
 	var fucjk string
-	fucjk =  fuck.time2str("2017-10-31/23:23")
+	fucjk =  fuck.Time2str("2017-10-31/23:23")
 	fmt.Println(fucjk)
 }
